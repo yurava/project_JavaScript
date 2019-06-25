27130172ccc37ac4cd263c610877f2a9fc28e3c1
Yuriy <live.in.ua@gmail.com>
2019-06-25 17:16:28 +0300

    zavd1

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
 'use strict';
 
 
-/*
-var number = 5;
-var string = "Hello!";
-var sym = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
 
-console.log(4/0);
-console.log('string'*9);
 
-let something;
-console.log(something);
 
-let persone = {
-    name: "John",
-    age: 25,
-    isMarried: false
-};
+//var time;
 
-console.log(persone["name"]);
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let rashod = prompt("Введите обязательную статью расходов в этом месяце");
+let oboyd = prompt("Во сколько обойдется?");
 
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-console.log(arr[1]);
-
-//alert("Hello!");
-let answer = prompt("Есть ли Вам 18?", "Да");
-console.log(typeof(answer));
-*/
-let incr = 9,
-    decr = 10;
-
-    incr++;
-    decr--;
-    console.log(incr);
-    console.log(decr);
+let appData = {
+    money: money,
+    timeData: time,
+    expenses: {
+rashod : oboyd
+    },
+    optionalExpenses,
+    income,
+    savings: false
+   };
+alert(money/30);
+console.log(appData["money"]);
+console.log(appData["timeData"]);
