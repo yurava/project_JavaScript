f52edf263ed8d545855a032932b7262b9385ca5e
Yuriy <live.in.ua@gmail.com>
2019-06-25 09:12:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+/*
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[1]);
+
+//alert("Hello!");
+let answer = prompt("Есть ли Вам 18?", "Да");
+console.log(typeof(answer));
+*/
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+    console.log(incr);
+    console.log(decr);
